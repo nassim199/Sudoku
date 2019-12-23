7811fd9f5786798064ee5d95104491772dd14e61
nassim199 <hammadounassim0@gmail.com>
2019-12-23 23:42:20 +0100

    added a timer (it has a problem) + numpad + navigating in the grid wih the narrow keys

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
   import SudokuGrid from "./components/SudokuGrid.svelte";
   import Numpad from "./components/Numpad.svelte";
+  import Timer from "./components/Timer.svelte";
   
   import { game, solution } from "./Sudoku games/sudoku_games.js";
-  export let name;
 
   let message = "Hello";
   let grid = game;
@@ -18,40 +18,58 @@
     };
   }
 
-  let gridEvaluation = [
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true, true]
-    ];
+  // let gridEvaluation = [
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true],
+  //     [true, true, true, true, true, true, true, true, true]
+  //   ];
+  let gridEvaluation = Array(9).fill(Array(9).fill(true));
 
-  document.addEventListener("keyup", event => {
-    if (event.keyCode >= 48 && event.keyCode <= 57) {
-      grid[selectedCell.i][selectedCell.j] = event.keyCode - 48;
+  function selectNumber(i) {
+
+    if (i >= 48 && i <= 57) {
+      grid[selectedCell.i][selectedCell.j] = i - 48;
         gridEvaluation[selectedCell.i][selectedCell.j] = 
         grid[selectedCell.i][selectedCell.j] === solution[selectedCell.i][selectedCell.j];
-    } else if (event.keyCode === 32) {
+    } else if (i === 32) {
       grid[selectedCell.i][selectedCell.j] = 0;
       gridEvaluation[selectedCell.i][selectedCell.j] = true;
+    } else if (i === 38) {
+      if (selectedCell.i !== 0) selectedCell.i--;
+    } else if (i === 37) {
+      if (selectedCell.j !== 0) selectedCell.j--;
+    } else if (i === 39) {
+      if (selectedCell.j !== 8) selectedCell.j++;
+    } else if (i === 40) {
+      if (selectedCell.i !== 8) selectedCell.i++;
     } else {
       return;
     }
 
     grid = grid;
+    selectedCell = selectedCell;
+  }
+
+  document.addEventListener("keydown", event => {
+    selectNumber(event.keyCode);
   });
+
+  let start = false;
+
 </script>
 
 <style>
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
   }
 
   h1 {
@@ -62,15 +80,35 @@
   }
 
   div {
-    /* margin: auto; */
-    text-align: center;
+    display: table-cell;
+    margin: 0 auto;
+    /* text-align: center; */
+  }
+
+  #grid::after {
+    width: 50vw;
+    height: 50vh;
+    background-color: antiquewhite;
+    content: 'Continue';
   }
 </style>
 
 <main>
   <h1>{message}!</h1>
-  <div>
+  <div id="grid">
+  {#if start}
     <SudokuGrid {grid} {selectedCell} gridEvaluation={gridEvaluation} on:select={selectCell} />
+  {:else} 
+    <button on:click={() => start = true}>Start Playing</button>
+  {/if}
   </div>
-    <Numpad />
+  <div>
+    <Numpad on:selectNumber={e => selectNumber(e.detail.j)}/>
+  </div>
+  <div id="timer">
+    <Timer bind:startTimer={start}/>
+    {#if start}
+    <button on:click={() => start = false}> Pause </button>
+    {/if}
+  </div>
 </main>
